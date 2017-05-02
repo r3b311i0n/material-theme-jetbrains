d8c999bc3728c0d8c7c7438ebafaf1d7d9db47ba
mallowigi <heliosaian@gmail.com>
2017-05-02 13:00:59 +0300

    remove status bars borders

--- src/main/java/com/chrisrm/idea/utils/UIReplacer.java
+++ src/main/java/com/chrisrm/idea/utils/UIReplacer.java
@@ -3,22 +3,36 @@ package com.chrisrm.idea.utils;
 import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
+import java.lang.reflect.Field;
 
 public class UIReplacer {
-  public static void patchUI() {
-    try {
-      Patcher.patchTables();
+    public static void patchUI() {
+        try {
+            Patcher.patchTables();
+            Patcher.patchStatusBar();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
 
-  static class Patcher {
-    static void patchTables() throws Exception {
-      StaticPatcher.setFinalStatic(UIUtil.class, "DECORATED_ROW_BG_COLOR", UIManager.get("Table.stripedBackground"));
+    static class Patcher {
+        static void patchTables() throws Exception {
+            StaticPatcher.setFinalStatic(UIUtil.class, "DECORATED_ROW_BG_COLOR", UIManager.get("Table.stripedBackground"));
+        }
+
+        static void patchStatusBar() throws Exception {
+            //        StaticPatcher.setFinalStatic(StatusBarUI.);
+            Class<?> clazz = Class.forName("com.intellij.openapi.wm.impl.status.StatusBarUI$BackgroundPainter");
+            Field border_top_color = clazz.getDeclaredField("BORDER_TOP_COLOR");
+            Field border2_top_color = clazz.getDeclaredField("BORDER2_TOP_COLOR");
+            Field border_bottom_color = clazz.getDeclaredField("BORDER_BOTTOM_COLOR");
+
+            StaticPatcher.setFinalStatic(border_top_color, UIManager.getColor("StatusBar.topColor").brighter().brighter());
+            StaticPatcher.setFinalStatic(border2_top_color, UIManager.getColor("StatusBar.topColor2"));
+            StaticPatcher.setFinalStatic(border_bottom_color, UIManager.getColor("StatusBar.bottomColor"));
+        }
     }
-  }
 
 
 }
